782cd146736ef1a987bb1ec46c42b0a45aebc45c
ILeenI <163290874+ILeenI@users.noreply.github.com>
2024-03-21 14:02:00 +0300

    All files after development and changes

---
+++ index1.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+
+ function myfunction(){
+    var a=10;
+    let b=10;
+    const c=10;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+ }
+ myfunction()
+ console.log(a)
---
+++ index2.js
@@ -0,0 +1,33 @@
+// const obj= [
+// {
+//     label:'a',
+//     value: 20
+// },
+
+// {
+//     label:'b',
+//     value: 30
+
+// },
+// {
+//     label:'c',
+//     value: 25
+// }
+// ];
+// // obj[1].value = 31;
+// // console.log(obj);
+// const updatedObj = obj.map(item => {
+//     if (item.label === 'b') {
+//         return { ...item, value: 31 }; 
+//     }
+//     return item;
+// });
+// console.log(updatedObj);
+
+const firstname="leen";
+console.log(firstname.charAt(0).toUpperCase() + firstname.slice(1));
+console.log(firstname.toUpperCase());
+
+let a = 3.14222
+let b = a.toFixed(2)
+console.log(b);
